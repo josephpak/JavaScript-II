9645616db16797b179b5bd1c49480275ac8fff87
joepak <josephpak2014@gmail.com>
2019-01-22 17:25:47 -0500

    Task 4

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,28 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function calculate (num1, num2) {
+  
+  return function addTwo() {
+    return num1 + num2;
+  }()
+
+}
+// calculate(1,2) // 3
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    count++
+    return count;
+  }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+// const newCounter = counter();
+// console.log(newCounter()); // 1 
+// console.log(newCounter()); // 2
+// console.log(newCounter());
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
